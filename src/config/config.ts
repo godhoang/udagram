e278abbdd97d1943c6454875198d420c53bc8045
godhoang <godhoangg@gmail.com>
2022-06-17 07:48:54 +0700

    update dev br with env var

---
+++ src/config/config.ts
@@ -0,0 +1,23 @@
+export const config = {
+    "dev": {
+      "username": "",
+      "password": "",
+      "database": "",
+      "host": "",
+      "dialect": "postgres",
+      "aws_region": "us-east-1",
+      "aws_profile": "default",
+      "aws_media_bucket": "udagram-godhoang-dev"
+    },
+    "jwt": {
+      "secret": " "
+    },
+    "prod": {
+      "username": "",
+      "password": "",
+      "database": "udagram_prod",
+      "host": "",
+      "dialect": "postgres"
+    }
+  }
+  
---
+++ www/config/config.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    "dev": {
+        "username": "",
+        "password": "",
+        "database": "",
+        "host": "",
+        "dialect": "postgres",
+        "aws_region": "us-east-1",
+        "aws_profile": "default",
+        "aws_media_bucket": "udagram-godhoang-dev"
+    },
+    "jwt": {
+        "secret": " "
+    },
+    "prod": {
+        "username": "",
+        "password": "",
+        "database": "udagram_prod",
+        "host": "",
+        "dialect": "postgres"
+    }
+};
+//# sourceMappingURL=config.js.map
